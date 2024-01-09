162bdfcac2e332baee2b43704f2418a7bede06d4
Ad-gangwar <adgangwar54@gmail.com>
2024-01-09 12:20:57 +0530

    added all songs

--- backend/Routes/song.js
+++ backend/Routes/song.js
@@ -30,6 +30,20 @@ router.get('/get/mySongs', passport.authenticate("jwt", { session: false }), asy
     return res.status(200).json({ data: songs });
 });
 
+//get route to get all the songs
+router.get('/get/allSongs', async (req, res) => {
+    try {
+        const songs = await Song.find({}).populate("artist");
+        if (!songs) {
+            return res.status(404).json({ error: 'No songs found' });
+        }
+        return res.status(200).json({ data: songs });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
 //get route to get all songs that a particular artist had published
 router.get('/get/artist/:artistId', passport.authenticate("jwt", { session: false }), async (req, res) => {
     const artistId= req.params.artistId;
